481c8844788ce4a874b327cbe23a6cef79857510
Sandy <reachdsandy@gmail.com>
2020-11-06 01:13:20 +0530

    initial commit

---
+++ precache-manifest.878ed69b362dc4c136569a6c8cde0d68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb5e324c2c50446dab39fc919b9180ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26ef5d5559e80ca2e845",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "568212513452ce0a3420",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "26ef5d5559e80ca2e845",
+    "url": "/static/js/2.e6ed7308.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.e6ed7308.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "568212513452ce0a3420",
+    "url": "/static/js/main.06f79bf1.chunk.js"
+  },
+  {
+    "revision": "0980db0307ffad65803b",
+    "url": "/static/js/runtime-main.df5858f9.js"
+  }
+]);
